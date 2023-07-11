7e27fa94e5d3477901e4923c886885afba051e79
iamukn <n.u.kingsley@gmail.com>
2023-07-11 20:36:44 +0000

    hot fix

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
-    """Adds two values
-    Args:
-        a: a float
-        b: a float
-    Return: Sum of a and B
-    """
+
     return a + b
