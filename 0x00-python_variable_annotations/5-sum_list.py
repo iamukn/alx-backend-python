4ad90071df96730d507f1ecce921013e31f4fccc
iamukn <n.u.kingsley@gmail.com>
2023-07-11 21:32:15 +0000

    hot fix

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-
+from typing import List
 """Takes a list as an argument and returns their sum"""
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """sums the inpt_list and returns it"""
 
-    return sum(input_list)
+    return float(sum(input_list))
