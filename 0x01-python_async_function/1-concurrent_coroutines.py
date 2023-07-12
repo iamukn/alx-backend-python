d09d2932f73385855cc668fa3b53a8cc95883e03
iamukn <n.u.kingsley@gmail.com>
2023-07-12 22:10:07 +0000

    hot fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,6 +9,7 @@ from typing import List
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     delays = []
     res = []
@@ -20,4 +21,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         delay = await task
         delays.append(delay)
     return delays
-
