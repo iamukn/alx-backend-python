7473b458c29149c9b26d328dc1444a30953d4376
iamukn <n.u.kingsley@gmail.com>
2023-07-12 22:08:31 +0000

    asyncio as completed method

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""A coroutine"""
+
+
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    delays = []
+    res = []
+    for i in range(n):
+        data = wait_random(max_delay)
+        res.append(data)
+
+    for task in asyncio.as_completed((res)):
+        delay = await task
+        delays.append(delay)
+    return delays
+
