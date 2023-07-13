e99e0c74bd712919157d498ead83020febe2f40a
iamukn <n.u.kingsley@gmail.com>
2023-07-13 21:55:14 +0000

    async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,7 +9,7 @@ from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """ Returns a random number retrieved from a coroutine"""
     result = [i async for i in async_generator()]
     return result
