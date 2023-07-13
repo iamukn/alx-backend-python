5cc904f9235875a2eea19d5b8e205cb0ee5145a1
iamukn <n.u.kingsley@gmail.com>
2023-07-13 21:39:33 +0000

    yield keyword

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ from random import uniform
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """loops 10x and yield a random number b/w 0 & 10"""
     for i in range(10):
         await asyncio.sleep(1)
